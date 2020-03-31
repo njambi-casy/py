dacb1d2dc3eee5dab84b00182864bced542511fb
njambi-casy <njambi1998@gmail.com>
2020-03-31 18:12:29 +0300

    classes

---
+++ python/classes.py
@@ -0,0 +1,11 @@
+class Point():
+    """docstring for Points."""
+
+    def __init__(self, x, y):
+        #super(Point, self).__init__()
+        self.x = x
+        self.y = y
+
+p = Point(3, 5)
+print( p.x)
+print( p.y)
